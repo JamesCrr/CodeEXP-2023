13807e47b4e8f8eabcc08746179fa45ffdf6b5e0
Wx <g0546699m@gmail.com>
2023-06-16 14:20:42 +0800

    Changed ReactNativeFirebase to Firebase JS SDK
    Expo does not support ReactNativeFirebase so uninstalling that package and installing Firebase JS SDK instead

--- codeexp/src/screens/CreatePostScreen.js
+++ codeexp/src/screens/CreatePostScreen.js
@@ -1,11 +1,47 @@
-import { Box } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Box, HStack, Input, Stack, TextArea, Pressable } from "native-base";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
 
 const CreatePostScreen = () => {
+  const [postTitle, setPostTitle] = useState("");
+  const [postContent, setPostContent] = useState("");
+
+  const onSelectImageHandler = () => {
+    console.log("Pressed!");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>CreatePostScreen</Text>
-    </View>
+    <Box alignItems="center" w="100%" h={"100%"} marginTop={5}>
+      <Stack w="90%" space={"md"}>
+        <Input
+          size={"md"}
+          placeholder="Post Title"
+          onChangeText={(text) => {
+            setPostTitle(text);
+          }}
+        />
+        <TextArea
+          h={400}
+          placeholder="Post Content"
+          onChangeText={(text) => {
+            setPostContent(text);
+          }}
+        />
+      </Stack>
+      <HStack
+        space={"md"}
+        w="90%"
+        // borderWidth={"1"}
+        // borderColor={"red"}
+        justifyContent={"flex-start"}
+        marginTop={2}
+      >
+        <Pressable onPress={onSelectImageHandler}>
+          <MaterialIcons name="image" size={35} color="black" />
+        </Pressable>
+      </HStack>
+    </Box>
   );
 };
 const styles = StyleSheet.create({
