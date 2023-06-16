9bc5daa1440687dbc2818dda5efa07bb033ceb5c
Wx <g0546699m@gmail.com>
2023-06-16 12:04:17 +0800

    Added nativebase UI library

--- codeexp/App.js
+++ codeexp/App.js
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from "react-native";
@@ -7,10 +8,12 @@ import FirstScreen from "./src/screens/FirstScreen";
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: true }}>
-        <Stack.Screen name="FirstScreen" component={FirstScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: true }}>
+          <Stack.Screen name="FirstScreen" component={FirstScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
--- codeexp/src/screens/FirstScreen.js
+++ codeexp/src/screens/FirstScreen.js
@@ -1,10 +1,25 @@
+import { Box } from "native-base";
 import { StyleSheet, Text, View } from "react-native";
 
 const FirstScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Your first text is here</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Your first text is here</Text>
+    // </View>
+
+    <Box
+      p="2"
+      bg="primary.500"
+      _text={{
+        fontSize: "md",
+        fontWeight: "medium",
+        color: "warmGray.50",
+        letterSpacing: "lg",
+      }}
+      shadow={2}
+    >
+      This is a Box
+    </Box>
   );
 };
 const styles = StyleSheet.create({
