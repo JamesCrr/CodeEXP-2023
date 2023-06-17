0dd1ae6a149b49579e1c14055c88fa811a665463
Wx <g0546699m@gmail.com>
2023-06-17 15:26:47 +0800

    made firebase in own file

---
+++ codeexp/src/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref as createDatabaseRef, set, push } from "firebase/database";
+import { getStorage, ref as createStorageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpLEaypKRTTzDJyvNVBFTW65qJtSgIdo8",
+  authDomain: "code-exp-borrowbuddy.firebaseapp.com",
+  databaseURL: "https://code-exp-borrowbuddy-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "code-exp-borrowbuddy",
+  storageBucket: "code-exp-borrowbuddy.appspot.com",
+  messagingSenderId: "952131407797",
+  appId: "1:952131407797:web:a52fd81aed2aa49ef2d3fc",
+  measurementId: "G-BHTXRF3ND9",
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
--- codeexp/src/screens/CreatePostScreen.js
+++ codeexp/src/screens/CreatePostScreen.js
@@ -4,30 +4,12 @@ import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref as createDatabaseRef, set, push } from "firebase/database";
-import { getStorage, ref as createStorageRef, uploadBytes, getDownloadURL } from "firebase/storage";
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBpLEaypKRTTzDJyvNVBFTW65qJtSgIdo8",
-  authDomain: "code-exp-borrowbuddy.firebaseapp.com",
-  databaseURL: "https://code-exp-borrowbuddy-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "code-exp-borrowbuddy",
-  storageBucket: "code-exp-borrowbuddy.appspot.com",
-  messagingSenderId: "952131407797",
-  appId: "1:952131407797:web:a52fd81aed2aa49ef2d3fc",
-  measurementId: "G-BHTXRF3ND9",
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Storage and get a reference to the service
-const storage = getStorage(app);
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+import { ref as createDatabaseRef, set, push } from "firebase/database";
+import { ref as createStorageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage, database } from "../Firebase";
 
 const appWidth = "90%";
-const CreatePostScreen = () => {
+const CreatePostScreen = ({ navigation }) => {
   const [postTitle, setPostTitle] = useState("");
   const [postContent, setPostContent] = useState("");
   const [validPost, setValidPost] = useState(false);
