6e1cb94d41663bccad75db6cb2adc9269d7eca64
MatthiasLJK <ljkmatthias@gmail.com>
2023-06-21 14:00:56 +0800

    update viewQuests to show workQuests

--- codeexp/src/screens/ViewQuests.js
+++ codeexp/src/screens/ViewQuests.js
@@ -23,31 +23,33 @@ const ViewQuests = () => {
   const dispatch = useAppDispatchContext();
   const [userWeeklyQuest, setUserWeeklyQuest] = useState([]);
   const [userMonthlyQuest, setUserMonthlyQuest] = useState([]);
-  // const [loaded, setLoaded] = useState(false);
+  const [userWorkQuest, setUserWorkQuest] = useState([]);
   const { userInfo } = useAppContext();
   const uid = userInfo.uid;
   let uncompletedQuests = [];
   let weeklyQuests = [];
   let monthlyQuests = [];
+  let workQuests = [];
 
   const goToCreatePostScreen = (item) => {
     console.log("item: " + Object.values(item), Object.keys(item));
-    console.log("item: " + item.questsId);
+    console.log("item: " + item.questId);
     navigation.navigate("CreatePostScreen");
-    dispatch({ type: "completedQuestId", val: item.questsId });
+    dispatch({ type: "completedQuestId", val: item.questId });
   };
 
   const displayQuests = async () => {
     const userQuestRef = doc(firestore, "users", uid);
     const userQuestSnap = await getDoc(userQuestRef);
+    console.log("userQuestSnap:", userQuestSnap.data());
     if (userQuestSnap.exists()) {
-      console.log("userQuestSnap:", userQuestSnap.data().socialQuest);
-      const quests = userQuestSnap.data().socialQuest;
+      console.log("userQuestSnap:", userQuestSnap.data().assignedQuest);
+      const quests = userQuestSnap.data().assignedQuest;
       console.log("quests:", quests);
       quests.map((key) => {
         console.log("key:", key.completed);
         if (key.completed === false) {
-          uncompletedQuests.push(key.questsId);
+          uncompletedQuests.push(key.questId);
         }
       });
       console.log("uncompletedQuests:", uncompletedQuests);
@@ -62,8 +64,8 @@ const ViewQuests = () => {
             console.log("HELLLLOOOOOO", questDescriptionSnap.data());
             if (questDescriptionSnap.exists()) {
               const questDescription = questDescriptionSnap.data();
-              if (questDescriptionSnap.data().duration === "weekly") {
-                questDescription.questsId = val[1];
+              if (questDescriptionSnap.data().duration === "weekly" && questDescriptionSnap.data().type !== "assigned") {
+                questDescription.questId = val[1];
                 weeklyQuests.push(questDescription);
                 console.log(
                   "questDescriptionWEEKLY:",
@@ -71,8 +73,8 @@ const ViewQuests = () => {
                 );
                 console.log("WEEKLY", weeklyQuests);
                 // setUserWeeklyQuest(weeklyQuests);
-              } else if (questDescriptionSnap.data().duration === "monthly") {
-                questDescription.questsId = val[1];
+              } else if (questDescriptionSnap.data().duration === "monthly" && questDescriptionSnap.data().type !== "assigned") {
+                questDescription.questId = val[1];
                 monthlyQuests.push(questDescription);
                 console.log(
                   "questDescriptionMONTHLY:",
@@ -81,6 +83,15 @@ const ViewQuests = () => {
                 console.log("MONTHLY:", monthlyQuests);
                 // setUserMonthlyQuest(monthlyQuests);
               }
+              else if(questDescriptionSnap.data().type === "assigned"){
+                questDescription.questId = val[1];
+                workQuests.push(questDescription);
+                console.log(
+                  "questDescriptionWORK:",
+                  questDescriptionSnap.data()
+                );
+                console.log("WORK:", workQuests);
+              }
             }
           });
         })
@@ -88,7 +99,7 @@ const ViewQuests = () => {
     } else {
       console.log("No such document!");
     }
-    return [weeklyQuests, monthlyQuests];
+    return [weeklyQuests, monthlyQuests, workQuests];
   };
 
   useEffect(() => {
@@ -99,15 +110,86 @@ const ViewQuests = () => {
       console.log("fetchedData[1]:", fetchedData[1]);
       setUserWeeklyQuest(fetchedData[0]);
       setUserMonthlyQuest(fetchedData[1]);
+      setUserWorkQuest(fetchedData[2]);
     });
   }, []);
 
   console.log("HELLO");
   console.log("userWeeklyQuest HERE:", userWeeklyQuest);
   console.log("userMonthlyQuest HERE:", userMonthlyQuest);
+  console.log("userWorkQuest HERE:", userWorkQuest);
 
   return (
     <Box alignItems="center">
+      <Heading fontSize="xl" pt="10">
+        Work Quests
+      </Heading>
+      <FlatList
+        data={userWorkQuest}
+        renderItem={({ item }) => (
+          <Box
+            borderBottomWidth="1"
+            _dark={{
+              borderColor: "muted.50",
+            }}
+            borderColor="muted.800"
+            pl={["0", "4"]}
+            pr={["0", "5"]}
+            py="2"
+          >
+            <HStack space={[2, 3]} justifyContent="space-between">
+              <VStack>
+                <Text
+                  _dark={{
+                    color: "warmGray.50",
+                  }}
+                  color="coolGray.800"
+                  bold
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  color="coolGray.600"
+                  _dark={{
+                    color: "warmGray.200",
+                  }}
+                >
+                  {item.description}
+                </Text>
+                <Text
+                  color="coolGray.600"
+                  _dark={{
+                    color: "warmGray.200",
+                  }}
+                >
+                  {item.currency}
+                </Text>
+                <Pressable onPress={() => goToCreatePostScreen(item)}>
+                  <Text
+                    color="coolGray.600"
+                    _dark={{
+                      color: "warmGray.200",
+                    }}
+                  >
+                    Perform Quest!
+                  </Text>
+                </Pressable>
+              </VStack>
+              <Spacer />
+              <Text
+                fontSize="xs"
+                _dark={{
+                  color: "warmGray.50",
+                }}
+                color="coolGray.800"
+                alignSelf="flex-start"
+              ></Text>
+            </HStack>
+          </Box>
+        )}
+        keyExtractor={(item, index) => index}
+      />
+
       <Heading fontSize="xl" pt="10">
         Weekly Quests
       </Heading>
@@ -180,6 +262,7 @@ const ViewQuests = () => {
       <Heading fontSize="xl" p="4" pb="3">
         Monthly Quests
       </Heading>
+      {console.log("userMONTHLYQUEST:", userMonthlyQuest)}
       <FlatList
         data={userMonthlyQuest}
         renderItem={({ item }) => (
